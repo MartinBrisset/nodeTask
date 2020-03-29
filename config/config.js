3a2580d9acb2c77ad993c5adbd2184d42ef75f11
martin <brissetmartin@gmail.com>
2020-03-29 12:12:32 -0300

    sacando variable duplicada

--- config/config.js
+++ config/config.js
@@ -8,5 +8,3 @@ process.env.MAIL_USER = process.env.MAIL_USER || '1cc34706a397ab';
 process.env.MAIL_PASS = process.env.MAIL_PASS || 'edb0c33381fe1c';
 process.env.MAIL_HOST = process.env.MAIL_HOST || 'smtp.mailtrap.io';
 process.env.MAIL_PORT = process.env.MAIL_PORT || '2525';
-
-process.env.HOST = process.env.HOST || 'localhost';
