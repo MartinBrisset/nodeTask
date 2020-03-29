c945145a6b0692632edb78f140c6118dc645ebb6
martin <brissetmartin@gmail.com>
2020-03-29 11:49:14 -0300

    agregando variables de entorno

---
+++ config/config.js
@@ -0,0 +1,12 @@
+process.env.BD_NOMBRE = process.env.BD_NOMBRE || 'uptasknode';
+process.env.BD_USER = process.env.BD_USER || 'root';
+process.env.BD_PASS = process.env.BD_PASS || '';
+process.env.BD_HOST = process.env.BD_HOST || '127.0.0.1';
+process.env.BD_PORT = process.env.BD_PORT || 3306;
+
+process.env.MAIL_USER = process.env.MAIL_USER || '1cc34706a397ab';
+process.env.MAIL_PASS = process.env.MAIL_PASS || 'edb0c33381fe1c';
+process.env.MAIL_HOST = process.env.MAIL_HOST || 'smtp.mailtrap.io';
+process.env.MAIL_PORT = process.env.MAIL_PORT || '2525';
+
+process.env.HOST = process.env.HOST || 'localhost';
